03b0714284cae51278c6b41f9a67374eda2cf294
阿仁 <oncwnuGZQI-OSXVF7ZcttBlH0bts@git.weixin.qq.com>
2021-10-12 17:31:22 +0800

    toy-js-03

--- toy_javascript/SyntaxParser.js
+++ toy_javascript/SyntaxParser.js
@@ -1,10 +1,10 @@
-import {scan} from './LexParser.js';
+import { scan } from './LexParser.js';
 // EOF ::= end of file
 let syntax = {
-    Program: [["StatementList","EOF"]],
+    Program: [["StatementList", "EOF"]],
     StatementList: [
         ["Statement"],
-        ["Statement","StatementList"]
+        ["Statement", "StatementList"]
     ],
     Statement: [
         ["ExpressionStatement"],
@@ -13,33 +13,33 @@ let syntax = {
         ["FunctionDeclaration"],
     ],
     IfStatement: [
-        ["if","(","Expression",")","Statement"]
+        ["if", "(", "Expression", ")", "Statement"]
     ],
-    VariableDeclaration:[
-        ["var","Identifier",";"],
-        ["let","Identifier",";"],
+    VariableDeclaration: [
+        ["var", "Identifier", ";"],
+        ["let", "Identifier", ";"],
     ],
     FunctionDeclaration: [
-        ["function","Identifier","(",")","{","StatementList","}"]
+        ["function", "Identifier", "(", ")", "{", "StatementList", "}"]
     ],
-    ExpressionStatement:[
-        ["Expression",";"]
+    ExpressionStatement: [
+        ["Expression", ";"]
     ],
     Expression: [
         ["AdditiveExpression"]
     ],
     AdditiveExpression: [
         ["MultiplicativeExpression"],
-        ["AdditiveExpression","+","MultiplicativeExpression"],
-        ["AdditiveExpression","-","MultiplicativeExpression"],
+        ["AdditiveExpression", "+", "MultiplicativeExpression"],
+        ["AdditiveExpression", "-", "MultiplicativeExpression"],
     ],
     MultiplicativeExpression: [
         ["PrimaryExpression"],
-        ["MultiplicativeExpression","*","PrimaryExpression"],
-        ["MultiplicativeExpression","/","PrimaryExpression"],
+        ["MultiplicativeExpression", "*", "PrimaryExpression"],
+        ["MultiplicativeExpression", "/", "PrimaryExpression"],
     ],
     PrimaryExpression: [
-        ["(","Expression",")"],
+        ["(", "Expression", ")"],
         ["Literal"],
         ["Identifier"],
     ],
@@ -52,33 +52,40 @@ let hash = {
 
 }
 
-function closure(state){
+function closure(state) {
     let queue = [];
     hash[JSON.stringify(state)] = state;
-    for(let symbol in state){
+    for (let symbol in state) {
+        // filter reduceType
+        if(symbol.match(/^\$/))
+            return;
         // Enqueue
         queue.push(symbol);
     }
-    while(queue.length){
+    while (queue.length) {
         // Dequeue
         let symbol = queue.shift();
-        if(syntax[symbol]){
-            for(let rule of syntax[symbol]){
-                if(!state[rule[0]])
+        if (syntax[symbol]) {
+            for (let rule of syntax[symbol]) {
+                if (!state[rule[0]])
                     queue.push(rule[0]);
                 let current = state;
-                for(let part of rule){
-                    if(!current[part])
+                for (let part of rule) {
+                    if (!current[part])
                         current[part] = {};
                     current = current[part];
                 }
-                current.$isRuleEnd = true;
-            } 
+                current.$reduceType = symbol;
+                current.$reduceLength = rule.length;
+            }
         }
     }
 
-    for(let symbol in state){
-         if(hash[JSON.stringify(state[symbol])])
+    for (let symbol in state) {
+        // filter reduceType
+        if(symbol.match(/^\$/))
+            return;
+        if (hash[JSON.stringify(state[symbol])])
             state[symbol] = hash[JSON.stringify(state[symbol])];
         else closure(state[symbol]);
     }
@@ -88,7 +95,7 @@ let end = {
     $isEnd: true,
 };
 let start = {
-    "Program":end
+    "Program": end
 }
 
 closure(start);
@@ -97,17 +104,44 @@ let source = (`
     let a;
 `)
 
-function parse(source){
-    let state = start;
-    for(let symbol of scan(source)){
-        if(symbol.type in state){
-            console.log(state);
-            state = state[symbol.type];
+function parse(source) {
+    let stack = [start];
+    let symbolStack = [];
+    function reduce(){
+        let state = stack[stack.length-1];
+
+        if(state.$reduceType){
+            let children = [];
+            for(let i = 0;i<state.$reduceLength;i++){
+                stack.pop()
+                children.push(symbolStack.pop());
+            }
+
+            // create a non-terminal symbol and shift it
+            return {
+                type: state.$reduceType,
+                children:children.reverse(),
+            }
+        }else{
+            throw new Error('unexpected token')
         }
-        else{
-            debugger
+        
+    }
+    function shift(symbol){
+        let state = stack[stack.length - 1];
+        if (symbol.type in state) {
+            stack.push(state[symbol.type]);
+            symbolStack.push(symbol);
+        } else {
+            /*reudce to non-terminal symbol*/
+            shift(reduce());
+            shift(symbol);
         }
     }
+    for (let symbol/*terminal symbol*/ of scan(source)) {
+        shift(symbol);
+    }
+    return reduce();
 }
 
 parse(source);
